14b31d3f4fd43fc80195760a34d0ae9d530c7700
Suzan Sucro <suzanmsucro@gmail.com>
2019-06-01 11:13:35 -0400

    Add proper map projection

--- assets/js/leaflet-hardiness.js
+++ assets/js/leaflet-hardiness.js
@@ -4,19 +4,20 @@
 
 const map = L.map('hardiness-map').setView([37.8, -96], 4);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic3V6bWFzIiwiYSI6ImNqdzlzbHY4ZjAybmEzeW10Z3dzcDY2cW4ifQ.VD3bq_R_nYPL8snODbm1cw', {
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic3V6bWFzIiwiYSI6ImNqdzlzbHY4ZjAybmEzeW10Z3dzcDY2cW4ifQ.VD3bq_R_nYPL8snODbm1cw', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1Ijoic3V6bWFzIiwiYSI6ImNqdzlzbHY4ZjAybmEzeW10Z3dzcDY2cW4ifQ.VD3bq_R_nYPL8snODbm1cw',
 }).addTo(map);
 
-const ophzStyle = {
-  fillColor: 'blue',
-  fillOpacity: 0.3,
-  weight: 0.5,
-  color: '#000',
-};
+function ophzStyle(feature) {
+  return {
+    fillColor: getColor(feature.properties.DN),
+    fillOpacity: 0.8,
+    weight: 0,
+    color: '#000',
+  };
+}
 
 const geoLayer = new L.geoJSON();
 geoLayer.addTo(map);
@@ -36,60 +37,43 @@ function addGeojson() {
 
 addGeojson();
 
-function interpolateColor(color1, color2, factor) {
-  if (arguments.length < 3) { 
-      factor = 0.5; 
-  }
-  var result = color1.slice();
-  for (var i = 0; i < 3; i++) {
-      result[i] = Math.round(result[i] + factor * (color2[i] - color1[i]));
-  }
-  return result;
-};
-// My function to interpolate between two colors completely, returning an array
-function interpolateColors(color1, color2, steps) {
-  var stepFactor = 1 / (steps - 1),
-      interpolatedColorArray = [];
 
-  color1 = color1.match(/\d+/g).map(Number);
-  color2 = color2.match(/\d+/g).map(Number);
-
-  for(var i = 0; i < steps; i++) {
-      interpolatedColorArray.push(interpolateColor(color1, color2, stepFactor * i));
-  }
-
-  return interpolatedColorArray;
+function scale(domain, range) {
+  const dMin = domain[0];
+  const dMax = domain[1];
+  const rMin = range[0];
+  const rMax = range[1];
+  return value => rMin + (rMax - rMin)
+                  * ((value - dMin) / (dMax - dMin));
 }
 
-var scale = function(opts){
-  var istart = opts.domain[0],
-      istop  = opts.domain[1],
-      ostart = opts.range[0],
-      ostop  = opts.range[1];
+const scaled = scale([-40, 40], [0, 100]);
+const color1 = [69, 117, 180];
+const color2 = [239, 195, 99];
+const color3 = [165, 0, 38];
 
-  return function scale(value) {
-    return ostart + (ostop - ostart) * ((value - istart) / (istop - istart));
-  }
-};
-
-function transitionRGB(val, max, start, end) {
+function transitionRGBChannel(val, max, start, end) {
   return start + (end - start) * val / max;
 }
 
-function transition3(val, max, start, end) {
-  const r1 = transitionRGB(val, max, start[0], end[0]);
-  const r2 = transitionRGB(val, max, start[1], end[1]);
-  const r3 = transitionRGB(val, max, start[2], end[2]);
+function transitionRGB(val, max, start, end) {
+  const r1 = transitionRGBChannel(val, max, start[0], end[0]);
+  const r2 = transitionRGBChannel(val, max, start[1], end[1]);
+  const r3 = transitionRGBChannel(val, max, start[2], end[2]);
   return [r1, r2, r3];
 }
 
-
 // ex input transition3mid(35, 70, [69, 117, 180], [239, 195, 99], [165, 0, 38])
-// also random http://strimas.com/r/ebird-county/
 function transition3mid(val, max, start, mid, end) {
   const midPoint = max / 2;
   if (val < midPoint) {
-    return transition3(val, midPoint, start, mid);
+    return transitionRGB(val, midPoint, start, mid);
   }
-  return transition3(val - midPoint, midPoint, mid, end);
+  return transitionRGB(val - midPoint, midPoint, mid, end);
+}
+
+function getColor(val) {
+  const scaledVal = scaled(val);
+  const rgb = `rgb(${(transition3mid(scaledVal, 100, color1, color2, color3)).join(',')}`;
+  return rgb;
 }
